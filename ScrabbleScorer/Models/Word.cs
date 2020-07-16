b2593b84348d72813480fc25a578894df7150899
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-16 11:55:33 -0700

    show a passing test for the input string returning as output
    Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com>

--- ScrabbleScorer/Models/Word.cs
+++ ScrabbleScorer/Models/Word.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace ScrabbleScorer.Models
 {
   public class Word
   {
+    public string Input { get; }
+
+    public Word(string input)
+    {
+      Input = input;
+    }
+    // public string Description { get; set; }
 
     // properties, methods, etc. will go here.
 
